5cc504eb194ffcc645cca5b502b30f15eea6067b
ssum-dal <min8226@gmail.com>
2021-05-08 22:58:54 +0900

    zip download

--- server/src/controllers/imageController.js
+++ server/src/controllers/imageController.js
@@ -1,6 +1,9 @@
 import routes from "../routes";
 import Image from "../models/Image";
+import AdmZip from "adm-zip";
+import fs from "fs";
 import path_ from "path";
+var uploadDir = fs.readdirSync(__dirname+"/../imageServer"); 
 
 export const home = (req, res) => {
   //res.send("Home");
@@ -22,7 +25,28 @@ export const upload = async (req, res) => {
   res.redirect(routes.home);
 };
 
-export const downloadImage = (req, res) => res.send({'image' : "https://drive.google.com/uc?id=1vqE7bvY71WV2-dUKk-8f5HYruUern3aJ", 'id' : 'dal'});
+export const downloadImage = (req, res) => {
+  const zip = new AdmZip();
+
+  for(var i = 0; i < uploadDir.length;i++){
+    zip.addLocalFile(__dirname+"/../imageServer/"+uploadDir[i]);
+}
+
+  // Define zip file name
+  const downloadName = `${Date.now()}.zip`;
+
+  const data = zip.toBuffer(); 
+
+  // save file zip in root directory
+  zip.writeZip(__dirname+"/../"+downloadName);
+
+  // code to download zip file
+
+  res.set('Content-Type','application/octet-stream');
+  res.set('Content-Disposition',`attachment; filename=${downloadName}`);
+  res.set('Content-Length',data.length);
+  res.send(data);
+}
 
 export const imageDetail = (req, res) => res.send("Image Detail");
 export const deleteImage = (req, res) => res.send("Delete Image");
