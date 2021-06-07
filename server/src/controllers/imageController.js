ab218867f24797851cf21d834df3e48b6225e59c
ssum-dal <min8226@gmail.com>
2021-06-07 22:25:10 +0900

    업로드 시 이미지 Object 반환

--- server/src/controllers/imageController.js
+++ server/src/controllers/imageController.js
@@ -1,14 +1,13 @@
-import routes from "../routes";
 import Image from "../models/Image";
-import aws from "aws-sdk";
 import fs from "fs";
 import s3Zip from "s3-zip";
 import path from "path";
 
-const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_KEY,
-  secretAccessKey: process.env.AWS_PRIVATE_KEY
-});
+
+const findDBImage = async(name) =>{
+  const img = await Image.findOne({ fileName: name });
+  return img;
+}
 
 export const home = (req, res) => {
 
@@ -19,30 +18,30 @@ export const home = (req, res) => {
 export const images = (req, res) => res.send("Images");
 
 export const upload = async (req, res) => {
-  Promise.all(
+  const Image_Object = [];
+
+  await Promise.all(
     req.files.map(async (file) => {
-      
+      const img = await findDBImage(file.originalname);
+
+      if(img){
+        return await img.save();
+      }
+  
       const newImage = new Image({
         fileUrl: file.location,
         fileName: file.originalname
       });
+      Image_Object.push({fileUrl: file.location, fileName: file.originalname});
       return await newImage.save();
     })
+
   );
-  res.json(req.files);//
+  res.json(Image_Object);
   console.log("이미지 업로드");
 };
 
 export const downloadImage = (req, res) => {
-  /*res.setHeader("content-type", "some/type");
-
-  const params = {
-    Bucket : "project-annotation/test",
-    Key: "dal.png"
-  };
-  
-  var stream = s3.getObject(params).createReadStream("dal.png");
-  stream.pipe(res);*/
   const region = "ap-northeast-2";
   const bucket = "project-annotation";
   const folder =  "test/";
@@ -52,14 +51,11 @@ export const downloadImage = (req, res) => {
 
   res.set('content-type', 'application/zip')
 
-  //const output = fs.createWriteStream(path.join(__dirname, 'hahahah.zip'));
+  //const output = fs.createWriteStream(path.join(__dirname, 'download.zip'));
 
-  s3Zip
-  .archive({ s3 : s3, bucket: bucket}, folder, [file1, file2, file3])
+  const archive = s3Zip
+  .archive({ region: region, bucket: bucket}, folder, [file1, file2, file3])
   .pipe(res)
-
-  //res.redirect(routes.home);
-
 }
 
 export const imageDetail = (req, res) => res.send("Image Detail");
