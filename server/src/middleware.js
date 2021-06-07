9ab706d5b973d1501e8ec93cf4fcfe890e7e3fee
ssum-dal <min8226@gmail.com>
2021-06-07 22:25:39 +0900

    config 추가로 인한 수정

--- server/src/middleware.js
+++ server/src/middleware.js
@@ -1,14 +1,7 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
-import aws from "aws-sdk";
+import s3 from "./config/s3";
 import routes from "./routes";
-
-const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_KEY,
-  secretAccessKey: process.env.AWS_PRIVATE_KEY,
-  region: "ap-northeast-2"
-});
-
 
 const multerImage = multer({
   storage: multerS3({
