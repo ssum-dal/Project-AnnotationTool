9055ead7836d31238d8870a5a44bf03f6082c822
ssum-dal <min8226@gmail.com>
2021-06-12 00:41:17 +0900

    .

--- server/src/routers/imageRouter.js
+++ server/src/routers/imageRouter.js
@@ -3,7 +3,6 @@ import routes from "../routes";
 import {
   images,
   upload,
-  imageDetail,
   downloadImage,
   deleteImage,
 } from "../controllers/imageController";
@@ -14,7 +13,6 @@ const imageRouter = express.Router();
 imageRouter.get(routes.images, images);
 imageRouter.post(routes.upload, uploadImage, upload);
 imageRouter.get(routes.downloadImage, downloadImage);
-imageRouter.get(routes.imageDetail, imageDetail);
 imageRouter.get(routes.deleteImage, deleteImage);
 
 export default imageRouter;
